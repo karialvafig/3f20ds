cdb6cbb19cedc6171ceeba036c30d1f7411e2664
karialvafig <72876737+karialvafig@users.noreply.github.com>
2020-11-12 21:18:51 -0600

    a ver si ya

--- 1p/ej/series/ej1.js
+++ 1p/ej/series/ej1.js
@@ -2,7 +2,7 @@ const x = 10;
 let cont = 0;
 let imparesEncontrados = 0;
 
-while(flag <= x) {
+while(imparesEncontrados <= x) {
     if(cont % 2 !== 0) {
         console.log(cont);
         imparesEncontrados++;
